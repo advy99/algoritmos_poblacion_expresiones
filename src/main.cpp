b0a212e8026e84f786d01abd5efcb7f670b91379
Antonio David Villegas <advy99@gmail.com>
2020-07-16 00:57:49 +0200

    update random number generator, now is a singleton

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,16 @@
 
 int main(int argc, char ** argv){
 
-	Random aleatorios;
+	Random * aleatorios = Random::getInstance(1234234323);
 
-	std::cout << aleatorios.getSeed() << std::endl;
+	std::cout << aleatorios->getSeed() << std::endl;
 
-	int aleatorio = aleatorios.getInt(20);
-	std::cout << aleatorio << std::endl;
+	int aleatorio = aleatorios->getInt(20);
+
+	std::cout << aleatorio  << std::endl;
 
 	for (int i = 0; i < aleatorio; i++){
-		std::cout << aleatorios.getFloat() << std::endl;
+		std::cout << aleatorios->getFloat(aleatorio) << std::endl;
 	}
 
 	return 0;
