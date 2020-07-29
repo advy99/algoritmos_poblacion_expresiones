44575a8e882aeee81df2fe8282821f504b36aab0
Antonio David Villegas <advy99@gmail.com>
2020-07-29 12:31:02 +0200

    comentamos el fichero nodo

--- src/nodo.cpp
+++ src/nodo.cpp
@@ -2,17 +2,21 @@
 #include "GA_P.hpp"
 
 void Nodo::setTerminoAleatorio(const int num_numeros, const int num_variables){
+	// si es un numero, escogemos un aleatorio entre todos los posibles valores dados
 	if (tipo_nodo == TipoNodo::NUMERO){
 		valor = Random::getInstance()->getInt(num_numeros);
 	} else {
 		// TO-DO
+		// si no, escogemos un aleatorio entre las posibles variables
 		valor = Random::getInstance()->getInt(num_variables);
 	}
 }
 
 void Nodo::setTipoNodoOperadorAleatorio(){
+	// obtenemos un aleatorio
 	float aleatorio = Random::getInstance()->getFloat();
 
+	// le damos la misma probabilidad a cada operador
 	if (aleatorio < 0.25){
 		tipo_nodo = TipoNodo::MAS;
 	} else if (aleatorio < 0.5) {
