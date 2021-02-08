bef389e727478b2a1bf3e7051a48ad4aad713688
Antonio David Villegas Yeguas <advy99@gmail.com>
2021-02-08 22:51:18 +0100

    fichero con funciones auxiliares para el espacio de nombres

---
+++ include/aux_gap.hpp
@@ -0,0 +1,17 @@
+/**
+  * @file aux_gap.hpp
+  * @brief Fichero con funciones auxiliares para el espacio de nombres GA_P
+  *
+  */
+
+#ifndef AUX_GAP
+#define AUX_GAP
+
+namespace GA_P {
+
+bool comparar_reales(const double a, const double b, const double epsilon = 0.005);
+
+}
+
+
+#endif
