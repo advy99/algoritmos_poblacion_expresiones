cb4727df7b4764f94da37eeff74d65aea103f632
Antonio David Villegas Yeguas <advy99@gmail.com>
2021-03-07 16:46:51 +0100

    el programa principal soporta introducir el numero de hilos de ejecucion

--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,27 @@
 #include "GA_P.hpp"
 #include <ctime>
 #include "random.hpp"
+#include <chrono>
 
+#ifdef _OPENMP
+	#include <omp.h>
+#endif
 
 
 int main(int argc, char ** argv){
 
-	if ( argc < 11 || argc > 12 ) {
+	if ( argc < 11 || argc > 13 ) {
 		std::cerr << "Error en el número de parámetros\n"
 					 << "\t Uso: " << argv[0] << " <fichero_datos> <tam_poblacion> <prob_variable> <profundidad_max> \n" 
-					 << "\t\t\t" << " <num_evaluaciones> <prob_cruce_gp> <prob_cruce_ga> <prob_mutacion_gp> <prob_mutacion_ga> <tam_torneo> [semilla]"
+					 << "\t\t\t" << " <num_evaluaciones> <prob_cruce_gp> <prob_cruce_ga> <prob_mutacion_gp> <prob_mutacion_ga> <tam_torneo> [num_trabajos] [semilla] "
 					 << std::endl;
 		exit(-1);
 	}
 
 	int semilla;
 
-	if ( argc == 12 ){
-		semilla = atoi(argv[2]);
+	if ( argc == 13 ){
+		semilla = atoi(argv[12]);
 	} else {
 		semilla = std::time(nullptr);
 	}
@@ -32,16 +36,30 @@ int main(int argc, char ** argv){
 	double prob_muta_gp = atof(argv[8]);
 	double prob_muta_ga = atof(argv[9]);
 	int tam_torneo = atoi(argv[10]);
+	
+	int num_trabajos = atoi(argv[11]);
 
+	#ifdef _OPENMP
+		omp_set_num_threads(num_trabajos);
+	#endif
 
 	GA_P::GA_P myGAP (std::string(argv[1]), '@', tam_pob, prob_variable, semilla, ',', prof_max_expr);
 
+	auto tiempo_inicio = std::chrono::high_resolution_clock::now();
 	
+	myGAP.ajustar(evaluaciones, prob_cruce_gp, prob_cruce_ga, prob_muta_gp, prob_muta_ga, tam_torneo, false);
 
-	myGAP.ajustar(evaluaciones, prob_cruce_gp, prob_cruce_ga, prob_muta_gp, prob_muta_ga, tam_torneo);
+	auto tiempo_fin = std::chrono::high_resolution_clock::now();
+
+	std::chrono::duration<double> t_ejecucion = std::chrono::duration_cast<std::chrono::microseconds>(tiempo_fin - tiempo_inicio);
+
+	std::cout << "Tiempo de ejecución con " << num_trabajos << " hilos en una poblacion de " << tam_pob << " individuos con tamaño máximo " 
+				 << prof_max_expr << " cada individuo y " << evaluaciones << " evaluaciones: " << t_ejecucion.count() << std::endl;
 
 
-	std::cout << myGAP.getMejorIndividuo() << std::endl;
+	// std::cout << "El mejor individuo es: " << std::endl;
+	// std::cout << myGAP.getMejorIndividuo() << std::endl;
+	// std::cout << "Con un ajuste de: " << myGAP.getMejorIndividuo().getFitness() << std::endl;
 
 	return 0;
 
