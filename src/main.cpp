072a54a14b5e9486498ba41153184c1c77b1c4bb
Antonio David Villegas Yeguas <advy99@gmail.com>
2021-03-15 17:04:06 +0100

    muestro el resultado final

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@ int main(int argc, char ** argv){
 
 	if ( argc < 11 || argc > 13 ) {
 		std::cerr << "Error en el número de parámetros\n"
-					 << "\t Uso: " << argv[0] << " <fichero_datos> <tam_poblacion> <prob_variable> <profundidad_max> \n" 
+					 << "\t Uso: " << argv[0] << " <fichero_datos> <tam_poblacion> <prob_variable> <profundidad_max> \n"
 					 << "\t\t\t" << " <num_evaluaciones> <prob_cruce_gp> <prob_cruce_ga> <prob_mutacion_gp> <prob_mutacion_ga> <tam_torneo> [num_trabajos] [semilla] "
 					 << std::endl;
 		exit(-1);
@@ -36,9 +36,11 @@ int main(int argc, char ** argv){
 	double prob_muta_gp = atof(argv[8]);
 	double prob_muta_ga = atof(argv[9]);
 	int tam_torneo = atoi(argv[10]);
-	
+
 	int num_trabajos = atoi(argv[11]);
 
+
+	// si utilizamos openMP, establecemos el número de trabajos
 	#ifdef _OPENMP
 		omp_set_num_threads(num_trabajos);
 	#endif
@@ -46,20 +48,20 @@ int main(int argc, char ** argv){
 	GA_P::GA_P myGAP (std::string(argv[1]), '@', tam_pob, prob_variable, semilla, ',', prof_max_expr);
 
 	auto tiempo_inicio = std::chrono::high_resolution_clock::now();
-	
+
 	myGAP.ajustar(evaluaciones, prob_cruce_gp, prob_cruce_ga, prob_muta_gp, prob_muta_ga, tam_torneo, false);
 
 	auto tiempo_fin = std::chrono::high_resolution_clock::now();
 
 	std::chrono::duration<double> t_ejecucion = std::chrono::duration_cast<std::chrono::microseconds>(tiempo_fin - tiempo_inicio);
 
-	std::cout << "Tiempo de ejecución con " << num_trabajos << " hilos en una poblacion de " << tam_pob << " individuos con tamaño máximo " 
+	std::cout << "Tiempo de ejecución con " << num_trabajos << " hilos en una poblacion de " << tam_pob << " individuos con tamaño máximo "
 				 << prof_max_expr << " cada individuo y " << evaluaciones << " evaluaciones: " << t_ejecucion.count() << std::endl;
 
 
-	// std::cout << "El mejor individuo es: " << std::endl;
-	// std::cout << myGAP.getMejorIndividuo() << std::endl;
-	// std::cout << "Con un ajuste de: " << myGAP.getMejorIndividuo().getFitness() << std::endl;
+	std::cout << "El mejor individuo es: " << std::endl;
+	std::cout << myGAP.getMejorIndividuo() << std::endl;
+	std::cout << "Con un ajuste de: " << myGAP.getMejorIndividuo().getFitness() << std::endl;
 
 	return 0;
 
