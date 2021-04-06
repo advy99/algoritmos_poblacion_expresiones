3b9b746dd33ba06144fba1e8a6837d8b0770db26
Antonio David Villegas Yeguas <advy99@gmail.com>
2021-04-06 20:35:54 +0200

    clase Parametros para gestionar los parametros de algoritmos

---
+++ include/algoritmos_poblaciones/Parametros.hpp
@@ -0,0 +1,49 @@
+#ifndef PARAMETROS_H_INCLUDED
+#define PARAMETROS_H_INCLUDED
+
+
+#include "algoritmos_poblaciones/aux_pg_algs.hpp"
+
+namespace algoritmos_poblaciones {
+
+class Parametros {
+	private:
+		int numero_evaluaciones_;
+		double probabilidad_cruce_gp_;
+		double probabilidad_cruce_ga_;
+		double probabilidad_mutacion_gp_;
+		double probabilidad_mutacion_ga_;
+		int tamanio_torneo_;
+		bool mostrar_evolucion_;
+
+	public:
+
+		Parametros(const int N_EVALS = 100000,
+			 		  const double PROB_CRUCE_GP = 0.8,
+					  const double PROB_CRUCE_GA = 0.8,
+					  const double PROB_MUTA_GP = 0.1,
+					  const double PROB_MUTA_GA = 0.1,
+					  const int TAM_TORNEO = 15,
+					  const bool MOSTRA_EVOLUCION = true);
+
+		Parametros(const int N_EVALS = 100000,
+			 		  const double PROB_CRUCE_GP = 0.8,
+					  const double PROB_MUTA_GP = 0.1,
+					  const int TAM_TORNEO = 15,
+					  const bool MOSTRAR_EVOLUCION = false);
+
+		int getNumeroEvaluaciones() const;
+		double getProbabilidadCruceGP() const;
+		double getProbabilidadCruceGA() const;
+
+		double getProbabilidadMutacionGP() const;
+		double getProbabilidadMutacionGA() const;
+
+		int getTamanioTorneo() const;
+		int getMostrarEvaluacion() const;
+
+};
+
+}
+
+#endif
