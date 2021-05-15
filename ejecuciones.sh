4e09ef9652135cea8b582c19311ebcb1cff26515
Antonio David Villegas Yeguas <advy99@gmail.com>
2021-05-15 19:21:57 +0200

    utilizo printf para poder usar secuencias de escape

--- ejecuciones.sh
+++ ejecuciones.sh
@@ -23,7 +23,7 @@ semillas=(12345 92034 8324 34679 34634)
 
 for profundidad in $(seq 20 20 60)
 do
-	echo "# semilla \t Error 5x2cv \t Mejor expresión " > salidas_ejecuciones/$(basename ${1})_prof_${profundidad}.dat
+	printf "# semilla \t Error 5x2cv \t Mejor expresión \n" > salidas_ejecuciones/$(basename ${1})_prof_${profundidad}.dat
 	for semilla in ${semillas[*]}
 	do
 		./bin/main $1 $TAM_POBLACION $PROB_VAR $profundidad $NUM_EVALS $PROB_CRUCE_GP $PROB_CRUCE_GA $PROB_MUTACION_GP $PROB_MUTACION_GA $PROB_CRUCE_INTRANICHO $TAM_TORNEO $NUM_HILOS $semilla >> salidas_ejecuciones/$(basename ${1})_prof_${profundidad}.dat &
