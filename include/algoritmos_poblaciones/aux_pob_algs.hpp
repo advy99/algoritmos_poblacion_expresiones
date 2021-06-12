a000fe2fdfe2a37c78d54836089af2c9e7b9dd00
Antonio David Villegas Yeguas <advy99@gmail.com>
2021-06-13 00:12:34 +0200

    cabecera del error absoluto medio

--- include/algoritmos_poblaciones/aux_pob_algs.hpp
+++ include/algoritmos_poblaciones/aux_pob_algs.hpp
@@ -80,6 +80,20 @@ double error_cuadratico_medio(const std::vector<double> & valores_predecidos,
 double raiz_error_cuadratico_medio(const std::vector<double> & valores_predecidos,
 											  const std::vector<double> & valores_reales);
 
+/**
+ * @brief Calcular el error absoluto medio entre un conjunto de valores predecidos y valores reales
+ *
+ * @param valores_predecidos Valores predecidos.
+ * @param valores_reales Valores reales.
+ *
+ * @return Error absoluto medio entre los valres predecidos y los reales
+ *
+ */
+
+double error_absoluto_medio(const std::vector<double> & valores_predecidos,
+									const std::vector<double> & valores_reales);
+
+
 }
 
 #include "preprocesado.hpp"
