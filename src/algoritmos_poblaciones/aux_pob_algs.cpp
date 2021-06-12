8fcbf738e026f42ebfc7855c65e168254ff626b3
Antonio David Villegas Yeguas <advy99@gmail.com>
2021-06-13 00:10:02 +0200

    pongo bien el MAE, que habia pisado el RECM

--- src/algoritmos_poblaciones/aux_pob_algs.cpp
+++ src/algoritmos_poblaciones/aux_pob_algs.cpp
@@ -24,27 +24,25 @@ double error_cuadratico_medio(const std::vector<double> & valores_predecidos,
 double raiz_error_cuadratico_medio(const std::vector<double> & valores_predecidos,
 											  const std::vector<double> & valores_reales) {
 
-	double resultado = 0.0;
-
-  	for ( unsigned i = 0; i < valores_reales.size(); i++ ) {
-  		resultado += std::abs(valores_predecidos[i] - valores_reales[i]);
-  	}
-
-  	resultado = resultado / static_cast<double>(valores_reales.size());
-
-	return resultado;
-
-}
-
-double error_absoluto_medio(const std::vector<double> & valores_predecidos,
-									 const std::vector<double> & valores_reales) {
-
 	double resultado = error_cuadratico_medio(valores_predecidos, valores_reales);
 	// resultado = RMSE
 	resultado = std::sqrt(resultado);
 
 	return resultado;
 
+}
+
+double error_absoluto_medio(const std::vector<double> & valores_predecidos,
+									const std::vector<double> & valores_reales) {
+
+	double resultado = 0.0;
+	for ( unsigned i = 0; i < valores_reales.size(); i++ ) {
+		resultado += std::abs(valores_predecidos[i] - valores_reales[i]);
+	}
+
+	resultado = resultado / static_cast<double>(valores_reales.size());
+
+	return resultado;
 }
 
 
